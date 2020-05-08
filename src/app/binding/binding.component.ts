0a9ec2a8adcfc3d1b9dd7636ba6f7efb0ee410aa
alexandruCochiorCorley <alexandru.cochior@corley.it>
2020-05-08 15:52:51 +0200

    Simple binding

---
+++ src/app/binding/binding.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'binding',
+  templateUrl: './binding.component.html'
+})
+export class BindingComponent  {
+  name: string = 'Pippo';
+
+
+}
