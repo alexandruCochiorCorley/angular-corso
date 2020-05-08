67183118161e36b94cb9254eff1c47d5141d549e
alexandruCochiorCorley <alexandru.cochior@corley.it>
2020-05-08 16:22:31 +0200

    1 file modified, 2 added

---
+++ src/app/bank/bank.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'binding',
+  templateUrl: './binding.component.html'
+})
+export class BindingComponent  {
+  public name: string = 'Pippo';
+
+  public changeName(name: string): void {
+    this.name = name;
+  }
+}
